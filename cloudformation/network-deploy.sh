e3d9470b121dd857d8c7b06ad4f0ae1232da45ed
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-01-12 16:52:55 -0500

    Refactoring

--- cloudformation/network-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-aws cloudformation deploy \
-  --template-file network.cf.yaml \
-  --stack-name pma-network \
-  --capabilities CAPABILITY_IAM \
-  --parameter-overrides $(jq -r '.[] | [.ParameterKey, .ParameterValue] | join("=")' network-params.json)
