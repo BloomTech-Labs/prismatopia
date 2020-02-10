7003ef62bb129167192e2e6f60a9e617dbedcc62
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-02-10 17:14:21 -0500

    Enhanced linting

--- apollo/src/index.js
+++ apollo/src/index.js
@@ -1,22 +1,21 @@
 // @ts-check
 
-const { importSchema } = require("graphql-import");
+const { importSchema } = require('graphql-import')
 
-const PORT = process.env.PORT || 8000;
-const { ApolloServer, gql } = require("apollo-server");
+const PORT = process.env.PORT || 8000
+const { ApolloServer, gql } = require('apollo-server')
 
-const resolvers = require("./resolvers");
-const context = require("./context");
+const resolvers = require('./resolvers')
+const context = require('./context')
 
+async function main () {
+  console.log('Importing schema')
 
-async function main() {
-  console.log("Importing schema")
-
-  const typeDefs = await importSchema("schema/schema.graphql")
+  const typeDefs = await importSchema('schema/schema.graphql')
 
   console.log(typeDefs)
 
-  console.log("Imported schema")
+  console.log('Imported schema')
 
   const server = new ApolloServer({
     resolvers,
@@ -24,11 +23,11 @@ async function main() {
     context,
     cors: true,
     dataSources: () => ({})
-  });
+  })
 
-  server.listen({port: PORT}).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+  server.listen({ port: PORT }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
 }
 
-main();
+main()
