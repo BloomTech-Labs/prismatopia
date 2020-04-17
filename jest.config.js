787b3048ffcd5b349f37320dd63e35dbcd11c6b0
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-04-17 16:48:45 -0400

    Build & test config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-// jest.config.js
-const {defaults} = require('jest-config');
 module.exports = {
   // This is a monorepo, so we need to tell Jest where the actual projects are
-  projects: ["apollo", "prisma"],
-};
+  projects: ['apollo']
+}
